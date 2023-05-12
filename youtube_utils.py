9492066b30a457be70c570fd2cf12f0c63ba6df7
Jan Misiurek <jan.misiurek@gmail.com>
2023-05-12 02:06:21 +0200

    fix download dir

--- youtube_utils.py
+++ youtube_utils.py
@@ -1,10 +1,13 @@
 from pytube import YouTube
 from rq.decorators import job
 from worker import conn
+import os
 
 @job('default', connection=conn, timeout=3600)
 def download_audio(url):
     yt = YouTube(url)
     audio_stream = yt.streams.filter(only_audio=True).first()
-    output_file = audio_stream.download(output_path='download')
+    output_dir = 'download'
+    os.makedirs(output_dir, exist_ok=True)  # utwórz katalog, jeśli nie istnieje
+    output_file = audio_stream.download(output_path=output_dir)
     return output_file
