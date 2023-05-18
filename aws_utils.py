c43cdb53aca0f102987b9fd7bc7cb0e3d1d1ee04
Jan Misiurek <jan.misiurek@gmail.com>
2023-05-18 22:35:08 +0200

    add aws s3 and railway postgres connections

---
+++ aws_utils.py
@@ -0,0 +1,63 @@
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ACCESS_KEY = os.getenv("ACCESS_KEY")
+SECRET_KEY= os.getenv("SECRET_KEY")
+
+def upload_to_s3(file_name, bucket, object_name=None):
+    print("ACCESS_KEY:", ACCESS_KEY)
+    print("SECRET_KEY:", SECRET_KEY)
+
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+    if object_name is None:
+        object_name = file_name
+
+    try:
+        s3.upload_file(file_name, bucket, object_name)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+    
+
+def list_files(bucket_name):
+    
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+    contents = []
+    try:
+        for item in s3.list_objects(Bucket=bucket_name)['Contents']:
+            contents.append(item['Key'])
+    except Exception as e:
+        print(e)
+        pass
+
+    return contents
+
+def create_presigned_url(bucket_name, object_name, expiration=3600):
+    
+    s3_client = boto3.client('s3', region_name='eu-central-1',
+                              aws_access_key_id=ACCESS_KEY,
+                              aws_secret_access_key=SECRET_KEY)
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        print(e)
+        return None
+
+    return response
+
+
