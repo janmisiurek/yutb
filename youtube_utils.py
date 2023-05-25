c58cc70f8c089c83ea76bfc43abef797b8d4d054
Jan Misiurek <jan.misiurek@gmail.com>
2023-05-25 22:34:50 +0200

    change yt-downloading lib

--- youtube_utils.py
+++ youtube_utils.py
@@ -1,21 +1,39 @@
-from pytube import YouTube
 from rq.decorators import job
 from worker import conn
 from aws_utils import upload_to_s3
 import os
+import yt_dlp
+
 
 @job('default', connection=conn, timeout=3600)
 def download_audio(url):
-    yt = YouTube(url)
-    audio_stream = yt.streams.filter(only_audio=True).first()
     output_dir = 'download'
     os.makedirs(output_dir, exist_ok=True)
-    output_file = audio_stream.download(output_path=output_dir)
 
-    # Extract filename from the full file path
-    output_file_name = os.path.basename(output_file)
-    output_s3_key = os.path.join(output_dir, output_file_name)
+    # Define options for yt-dlp
+    options = {
+        'format': 'bestaudio/best',
+        'outtmpl': os.path.join(output_dir, '%(id)s.%(ext)s'),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }],
+    }
+
+    # Download the audio
+    with yt_dlp.YoutubeDL(options) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info_dict)
+    
+    # Change filename to match original function
+    output_file = os.path.join(output_dir, info_dict['id'] + '.mp3')
+
+    if filename != output_file:
+        os.rename(filename, output_file)
+
     print('sending file to s3')
+    output_s3_key = os.path.join(output_dir, info_dict['id'] + '.mp3')
+    
     # Upload file to S3
     upload_to_s3(output_file, 'wiadroborka', object_name=output_s3_key)
 
