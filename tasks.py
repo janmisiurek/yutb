0a1a579b9c304f58e97f944f20608a709ae0fa23
Jan Misiurek <jan.misiurek@gmail.com>
2023-05-26 01:54:55 +0200

    move app instance

--- tasks.py
+++ tasks.py
@@ -2,13 +2,15 @@ from rq import Queue
 from rq.decorators import job
 from worker import conn
 from models import db, Transcription
-from flask import current_app
+
 import os
 
 q = Queue(connection=conn)
 
 @job('default', connection=conn, timeout=3600)
 def add_to_database(transcript_output):
+    from app import app
+
     transcript_text, transcript_file_path, audio_file_path, yt_url = transcript_output
     name_without_extension = os.path.splitext(os.path.basename(transcript_file_path))[0]
     name_without_extension = os.path.splitext(name_without_extension)[0]
@@ -16,11 +18,11 @@ def add_to_database(transcript_output):
     record = Transcription(
         name=name_without_extension,
         yt_url=yt_url, 
-        audio_url='download/' + name_without_extension + '.mp4', 
+        audio_url='download/' + name_without_extension + '.mp3', 
         transcript_url=transcript_file_path
     )
 
-    with current_app.app_context():
+    with app.app_context():
         db.session.add(record)
         db.session.commit()
         
