e358ee859a07cc3b53e9ed8d348542da199520b2
Jan Misiurek <jan.misiurek@gmail.com>
2023-05-27 13:25:48 +0200

    file name fix 1

--- youtube_utils.py
+++ youtube_utils.py
@@ -5,8 +5,6 @@ import os
 import yt_dlp
 import openai_utils
 
-def clean_title(title):
-    return "".join(c for c in title if c.isalnum() or c in (' ', '.', '_')).rstrip()
 
 @job('default', connection=conn, timeout=3600)
 def download_audio(url):
@@ -30,12 +28,12 @@ def download_audio(url):
         info_dict = ydl.extract_info(url, download=True)
         # Prepare the output file name by replacing the extension with .mp3
         output_file = ydl.prepare_filename(info_dict).replace('.webm', '.mp3')
-        clean_file_name = clean_title(output_file)
+
 
     print('sending file to s3')
-    output_s3_key = os.path.join(output_dir, clean_file_name)
+    output_s3_key = os.path.join(output_dir, output_file)
 
     # Upload file to S3
-    upload_to_s3(clean_file_name, 'wiadroborka', object_name=output_s3_key)
+    upload_to_s3(output_file, 'wiadroborka', object_name=output_s3_key)
 
-    return clean_file_name, url
+    return output_file, url
