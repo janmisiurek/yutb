07194fa7052b5b76745f0fdd0eb84ea0a87ae9ca
Jan Misiurek <jan.misiurek@gmail.com>
2023-06-13 14:12:46 +0200

    update of the database

--- models.py
+++ models.py
@@ -8,5 +8,15 @@ class Transcription(db.Model):
     yt_url = db.Column(db.String(256))
     audio_url = db.Column(db.String(256))
     transcript_url = db.Column(db.String(256))
-    notes_url_gpt3 = db.Column(db.String(256))
     notes_url_gpt4 = db.Column(db.String(256))
+
+class SocialMediaContent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    transcription_id = db.Column(db.Integer, db.ForeignKey('transcription.id'))
+    tweet_gpt35 = db.Column(db.Text)
+    tweet_thread_gpt35 = db.Column(db.Text)
+    linkedin_post_gpt35 = db.Column(db.Text)
+    tweet_gpt4 = db.Column(db.Text)
+    tweet_thread_gpt4 = db.Column(db.Text)
+    linkedin_post_gpt4 = db.Column(db.Text)
+    transcription = db.relationship('Transcription', backref=db.backref('social_media_contents', lazy=True))
