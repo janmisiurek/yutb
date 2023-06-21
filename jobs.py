8815f84997ffe22641b96738eca0980b995c55d2
Jan Misiurek <jan.misiurek@gmail.com>
2023-06-21 15:39:53 +0200

    add social meedia ccontent generation to main job

--- jobs.py
+++ jobs.py
@@ -1,12 +1,25 @@
 from rq.decorators import job
 from worker import conn
+from rq import Queue, Connection
+from rq.decorators import job
+from redis import Redis
+
 from youtube_utils import download_audio
-from openai_utils import transcript, generate_notes
+from openai_utils import transcript, generate_notes, generate_social_media_content
+
+redis_conn = Redis()
+conn = Connection(redis_conn)
+q = Queue(connection=conn)
 
 # Helper function combines the download, transcript, and create_notes functions for asynchronous execution as an RQ job
 @job('default', connection=conn, timeout=7200)
-def download_transcribe_generate_notes(url, tempo):
+def download_transcribe_generate_notes(url, tempo, content_types):
     record_id = download_audio(url, tempo)
     transcript(record_id)
     generate_notes(record_id)
+
+    if content_types:
+        q.enqueue(generate_social_media_content, record_id, content_types)
+ 
+
     return record_id
