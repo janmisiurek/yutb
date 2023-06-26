15a6b7319955235cf7faf5e5778b6153be19bc24
Jan Misiurek <jan.misiurek@gmail.com>
2023-06-27 01:58:47 +0200

    passing user id fix

--- jobs.py
+++ jobs.py
@@ -1,4 +1,4 @@
-from flask import current_app
+
 from rq.decorators import job
 from worker import conn
 from rq import Queue
@@ -13,14 +13,12 @@ q = Queue(connection=conn)
 
 # Helper function combines the download, transcript, and create_notes functions for asynchronous execution as an RQ job
 @job('default', connection=conn, timeout=7200)
-def download_transcribe_generate_notes(url, tempo, content_types, user_id):
-    with current_app.app_context():
-        user = User.query.get(user_id)
-        record_id = download_audio(url, tempo, user)
-        transcript(record_id)
-        generate_notes(record_id)
+def download_transcribe_generate_notes(url, tempo, content_types, user):
+    record_id = download_audio(url, tempo, user)
+    transcript(record_id)
+    generate_notes(record_id)
 
-        if content_types:
-            q.enqueue(generate_social_media_content, record_id, content_types)
+    if content_types:
+        q.enqueue(generate_social_media_content, record_id, content_types)
     
     return record_id
